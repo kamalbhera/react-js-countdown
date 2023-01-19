c1c170cbe73db4ebafdc3d778ddeebd16be4e37f
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-01-20 00:56:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(10);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let timeout = null
+    if (counter <= 0) {
+      return () => clearTimeout(timeout)
+    }
+    if (isActive) {
+      timeout = setTimeout(() => {
+        setCounter(counter - 1)
+      }, 1000);
+    }
+  }, [isActive, counter]);
+
+  function startTimer() {
+    setCounter(10);
+    setIsActive(true)
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +29,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <button
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          type="button"
+          onClick={() => startTimer()}
         >
-          Learn React
-        </a>
+          Start
+        </button>
+        <p>{counter}</p>
       </header>
     </div>
   );
